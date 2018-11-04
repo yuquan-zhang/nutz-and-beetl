473b8046e73e1f51912d9417dedc22316bfea6cf
zhangyong <zhytwo@126.com>
2018-11-04 10:38:30 +0800

    1,500页面

--- nutzandbeetl/src/main/java/com/zhang/yong/nutz/module/MainModule.java
+++ nutzandbeetl/src/main/java/com/zhang/yong/nutz/module/MainModule.java
@@ -4,14 +4,13 @@ import org.beetl.ext.nutz.BeetlViewMaker;
 import org.nutz.mvc.annotation.*;
 
 @SetupBy(MainSetup.class)
-@IocBy(args={"*js", "ioc/",
-        "*anno", "com.zhang.yong.nutz",
-        "*async",
-        "*tx"
-})
+@IocBy(args={"*js", "ioc/", "*anno", "com.zhang.yong.nutz", "*async", "*tx"})
 @Modules
 @Views(BeetlViewMaker.class)
 @ChainBy(args = "mvc/chain.js")
+@Ok("json:full")
+@Fail("jsp:jsp.500")
+@Localization(value="msg/", defaultLocalizationKey="zh-CN")
 public class MainModule {
 
 }
